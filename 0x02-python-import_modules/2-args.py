db542bf0f715414ef9388a6e4b05b60146a6d1ea
Luis Ernesto <noahverner@outlook.com>
2021-12-23 18:47:47 -0500

    This Python program prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) is 1:
+    if len(sys.argv) == 1:
         print("{:d} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) is 2:
+    elif len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
         print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
