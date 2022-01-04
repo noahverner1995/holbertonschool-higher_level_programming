0128a595f69df5ee055c80d3e81c4576e0ee5c99
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-01-03 22:26:58 -0500

    updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is "":
+        tup = (len(sentence), None)
+        return tup
+    else:
+        tup = (len(sentence), sentence[0])
+        return tup
