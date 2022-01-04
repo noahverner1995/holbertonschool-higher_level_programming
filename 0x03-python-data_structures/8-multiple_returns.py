174d180e277678b71ac2c63a5dc197d170669316
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-01-03 23:05:55 -0500

    This Python program has a function that returns a tuple with the leng
    th of a string and ist first character, if the sentence is empty, the first character should be equal to 'None'

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if sentence is "":
         tup = (len(sentence), None)
-        return None
+        return 'None'
     else:
         tup = (len(sentence), sentence[0])
         return tup
