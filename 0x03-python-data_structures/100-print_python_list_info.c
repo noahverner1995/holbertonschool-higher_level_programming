c4ae80989332d761d84aad87b86cb329d7979542
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-01-03 23:32:06 -0500

    betty style added

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,16 +11,14 @@ void print_python_list_info(PyObject *p);
  */
 void print_python_list_info(PyObject *p)
 {
-	PyObject *type = NULL;
-	int len = (int)PyList_Size(p);
-	int i;
-
-	printf("[*] Size of the Python List = %d\n", len);
-	printf("[*] Allocated = %d\n", (int)((PyListObject *)(p))->allocated);
-
-	for (i = 0; i < len; ++i)
-	{
-		type = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, (char *)Py_TYPE(type)->tp_name);
-	}
+PyObject *type = NULL;
+int len = (int)PyList_Size(p);
+int i;
+printf("[*] Size of the Python List = %d\n", len);
+printf("[*] Allocated = %d\n", (int)((PyListObject *)(p))->allocated);
+for (i = 0; i < len; ++i)
+{
+type = PyList_GetItem(p, i);
+printf("Element %d: %s\n", i, (char *)Py_TYPE(type)->tp_name);
+}
 }
