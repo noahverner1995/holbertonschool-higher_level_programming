d85eaa2ecac1abba51f45cb76f3602e1c6d2849e
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-01-05 23:54:49 -0500

    This Python program has a function that converts a 'Roman numeral' to
    an int, assume the number will be between 1 to 3999, and the function must return an integer only, if the 'roman_string' is not a str or 'None' return 0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,4 +2,15 @@
 def roman_to_int(roman_string):
     if isinstance(roman_string, str) == False or roman_string == None:     
         return 0
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}    
+    values={'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}  
+    """Convert from Roman numerals to an integer."""
+    numbers = []
+    for char in roman_string:
+        numbers.append(values[char]) 
+    total = 0
+    for num1, num2 in zip(numbers, numbers[1:]):
+        if num1 >= num2:
+            total += num1
+        else:
+            total -= num1
+    return total + num2
