6764e1411610e5fd51da4a9b6f385da0e0b30edf
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-01-05 23:10:34 -0500

    file created

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    big = max(a_dictionary.values())
+    return [k for k, v in a_dictionary.items() if big == v][0]
